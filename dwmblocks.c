808f813803796bd9b75fbce47d1453d1f8a79f25
Oscar Benedito <oscar@oscarbenedito.com>
2021-03-28 22:25:29 +0200

    Code cleanup

--- dwmblocks.c
+++ dwmblocks.c
@@ -7,14 +7,14 @@
 #include<X11/Xlib.h>
 #endif
 #ifdef __OpenBSD__
-#define SIGPLUS			SIGUSR1+1
-#define SIGMINUS		SIGUSR1-1
+#define SIGPLUS                 SIGUSR1+1
+#define SIGMINUS                SIGUSR1-1
 #else
-#define SIGPLUS			SIGRTMIN
-#define SIGMINUS		SIGRTMIN
+#define SIGPLUS                 SIGRTMIN
+#define SIGMINUS                SIGRTMIN
 #endif
 #define LENGTH(X)               (sizeof(X) / sizeof (X[0]))
-#define CMDLENGTH		50
+#define CMDLENGTH               50
 #define MIN( a, b ) ( ( a < b) ? a : b )
 #define STATUSLENGTH (LENGTH(blocks) * CMDLENGTH + 1)
 
@@ -47,7 +47,6 @@ static Window root;
 static void (*writestatus) () = pstdout;
 #endif
 
-
 #include "blocks.h"
 
 static char statusbar[LENGTH(blocks)][CMDLENGTH] = {0};
@@ -55,7 +54,7 @@ static char statusstr[2][STATUSLENGTH];
 static int statusContinue = 1;
 static int returnStatus = 0;
 
-//opens process *cmd and stores output in *output
+/* opens process *cmd and stores output in *output */
 void getcmd(const Block *block, char *output)
 {
 	strcpy(output, block->icon);
@@ -64,20 +63,14 @@ void getcmd(const Block *block, char *output)
 		return;
 	int i = strlen(block->icon);
 	fgets(output+i, CMDLENGTH-i-delimLen, cmdf);
+	pclose(cmdf);
 	i = strlen(output);
 	if (i == 0) {
-		//return if block and command output are both empty
-		pclose(cmdf);
+		/* return if block and command output are both empty */
 		return;
 	}
-	if (delim[0] != '\0') {
-		//only chop off newline if one is present at the end
-		i = output[i-1] == '\n' ? i-1 : i;
-		strncpy(output+i, delim, delimLen); 
-	}
-	else
-		output[i++] = '\0';
-	pclose(cmdf);
+	i = (i && output[i-1] == '\n') ? i-1 : i;   /* delete trailing newline */
+	strncpy(output+i, delim, delimLen);
 }
 
 void getcmds(int time)
@@ -86,7 +79,7 @@ void getcmds(int time)
 	for (unsigned int i = 0; i < LENGTH(blocks); i++) {
 		current = blocks + i;
 		if ((current->interval != 0 && time % current->interval == 0) || time == -1)
-			getcmd(current,statusbar[i]);
+			getcmd(current, statusbar[i]);
 	}
 }
 
@@ -96,23 +89,22 @@ void getsigcmds(unsigned int signal)
 	for (unsigned int i = 0; i < LENGTH(blocks); i++) {
 		current = blocks + i;
 		if (current->signal == signal)
-			getcmd(current,statusbar[i]);
+			getcmd(current, statusbar[i]);
 	}
 }
 
 void setupsignals()
 {
 #ifndef __OpenBSD__
-	    /* initialize all real time signals with dummy handler */
-    for (int i = SIGRTMIN; i <= SIGRTMAX; i++)
-        signal(i, dummysighandler);
+	/* initialize all real time signals with dummy handler */
+	for (int i = SIGRTMIN; i <= SIGRTMAX; i++)
+		signal(i, dummysighandler);
 #endif
 
 	for (unsigned int i = 0; i < LENGTH(blocks); i++) {
 		if (blocks[i].signal > 0)
 			signal(SIGMINUS+blocks[i].signal, sighandler);
 	}
-
 }
 
 int getstatus(char *str, char *last)
@@ -122,13 +114,14 @@ int getstatus(char *str, char *last)
 	for (unsigned int i = 0; i < LENGTH(blocks); i++)
 		strcat(str, statusbar[i]);
 	str[strlen(str)-strlen(delim)] = '\0';
-	return strcmp(str, last);//0 if they are the same
+	return strcmp(str, last);       /* 0 if they are the same */
 }
 
 #ifndef NO_X
 void setroot()
 {
-	if (!getstatus(statusstr[0], statusstr[1]))//Only set root if text has changed.
+	/* only set root if text has changed */
+	if (!getstatus(statusstr[0], statusstr[1]))
 		return;
 	XStoreName(dpy, root, statusstr[0]);
 	XFlush(dpy);
@@ -149,12 +142,12 @@ int setupX()
 
 void pstdout()
 {
-	if (!getstatus(statusstr[0], statusstr[1]))//Only write out if text has changed.
+	/* only write out if text has changed */
+	if (!getstatus(statusstr[0], statusstr[1]))
 		return;
-	printf("%s\n",statusstr[0]);
+	printf("%s\n", statusstr[0]);
 	fflush(stdout);
 }
-
 
 void statusloop()
 {
@@ -174,7 +167,7 @@ void statusloop()
 /* this signal handler should do nothing */
 void dummysighandler(int signum)
 {
-    return;
+	return;
 }
 #endif
 
@@ -191,10 +184,11 @@ void termhandler()
 
 int main(int argc, char** argv)
 {
-	for (int i = 0; i < argc; i++) {//Handle command line arguments
-		if (!strcmp("-d",argv[i]))
+	/* handle command line arguments */
+	for (int i = 0; i < argc; i++) {
+		if (!strcmp("-d", argv[i]))
 			strncpy(delim, argv[++i], delimLen);
-		else if (!strcmp("-p",argv[i]))
+		else if (!strcmp("-p", argv[i]))
 			writestatus = pstdout;
 	}
 #ifndef NO_X
